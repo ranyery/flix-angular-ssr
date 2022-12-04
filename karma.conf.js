b79bbbad431bebedb731df4255c10bb2d1d24e4b
ranyery <ranyerysc@gmail.com>
2022-12-04 02:42:24 -0300

    chore: change project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-boilerplate'),
+      dir: require('path').join(__dirname, './coverage/flixtorrent'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
