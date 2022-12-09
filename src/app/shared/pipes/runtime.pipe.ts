c563d5fbc49bf65f39991c82cc5efc4504c5ff1a
ranyery <ranyerysc@gmail.com>
2022-12-09 15:03:41 -0300

    refactor: change runtime pipe

--- src/app/shared/pipes/runtime.pipe.ts
+++ src/app/shared/pipes/runtime.pipe.ts
@@ -5,6 +5,17 @@ export class RuntimePipe implements PipeTransform {
   transform(value: number): string {
     const hours = Math.floor(value / 60);
     const minutes = value % 60;
-    return `${hours}h ${minutes}min`;
+
+    let result = null;
+
+    if (hours === 0) {
+      result = `${minutes}min`;
+    } else if (minutes === 0) {
+      result = `${hours}h`;
+    } else {
+      result = `${hours}h ${minutes}min`;
+    }
+
+    return result;
   }
 }
