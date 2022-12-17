39c9ca5540398c4588e98aebcc80b500d77a48dc
ranyery <ranyerysc@gmail.com>
2022-12-17 15:49:07 -0300

    feat(api): update service to user vercel api after build

--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -1,30 +1,37 @@
+import { isPlatformServer } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Observable } from 'rxjs';
+import { API_PATH } from 'src/environments/environment';
 
 import { IMovie } from '../interfaces/IMovie';
 import { FetchOrCacheService } from './fetch-or-cache.service';
 
 @Injectable({ providedIn: 'root' })
 export class MovieService {
-  private BASE_URL = '/api/movies';
+  private _isServer: boolean;
+  private _BASE_URL: string;
 
   constructor(
     private httpClient: HttpClient,
-    private fetchOrCacheService: FetchOrCacheService
-  ) {}
+    private fetchOrCacheService: FetchOrCacheService,
+    @Inject(PLATFORM_ID) platformId: Object
+  ) {
+    this._isServer = isPlatformServer(platformId);
+    this._BASE_URL = this._isServer ? `${API_PATH}/movies` : '/api/movies';
+  }
 
   getBySlug(slug: string): Observable<IMovie> {
     return this.fetchOrCacheService.set<IMovie>(
       slug,
-      this.httpClient.get<IMovie>(`${this.BASE_URL}/${slug}`)
+      this.httpClient.get<IMovie>(`${this._BASE_URL}/${slug}`)
     );
   }
 
   getAll(): Observable<any> {
     return this.fetchOrCacheService.set<IMovie>(
       'getAll',
-      this.httpClient.get<any>(this.BASE_URL)
+      this.httpClient.get<any>(this._BASE_URL)
     );
   }
 }
